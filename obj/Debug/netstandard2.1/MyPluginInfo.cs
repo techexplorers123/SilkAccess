b8d648cf798cff528a03ed9b560d3eb441d311ad
tech explorers <73522283+techexplorers123@users.noreply.github.com>
2025-09-22 10:53:14 +0530

    fully renamed to  SilkAccess

--- obj/Debug/netstandard2.1/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace MyFirstPlugin
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "MyFirstPlugin";
-        public const string PLUGIN_NAME = "My first plugin";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
